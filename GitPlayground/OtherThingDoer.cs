5532a3d0abf81153b58143c66794c2807d6be673
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-04-21 11:44:50 +0100

    Added kittens and frogs.

--- GitPlayground/OtherThingDoer.cs
+++ GitPlayground/OtherThingDoer.cs
@@ -9,18 +9,23 @@ namespace GitPlayground
             OutputInstructions();
             string input = System.Console.ReadLine();
 
-            while (input.ToUpper() != "EXIT")
+            while (input.ToUpper() != "BOGIES")
             {
                 OutputInstructions();
                 input = System.Console.ReadLine();
             }
 
-            System.Console.WriteLine("Goodbye!");
+            System.Console.WriteLine("Goodbye" + CatchAKitten());
+        }
+
+        private static string CatchAKitten()
+        {
+            return " Kitten.";
         }
 
         private static void OutputInstructions()
         {
-            System.Console.WriteLine("Let's do another thing! Enter 'Exit' to exit.");
+            System.Console.WriteLine("Let's do another thing! Enter 'BOGIES' to exit.");
         }
     }
 }
--- GitPlayground/Program.cs
+++ GitPlayground/Program.cs
@@ -45,11 +45,17 @@ namespace GitPlayground
             InputOutput.OutputOptions(new List<string>
                 {
                     "1. Do this thing here",
-                    "2. Do another thing!",
-                    "3. Exit",
+                    "2. No, do this other thing entirely!",
+                    "3. " + CatchAFrog(),
+                    "4. Exit",
                 });
 
             return System.Console.ReadLine();
+        }
+
+        private static string CatchAFrog()
+        {
+            return "A frog";
         }
     }
 }
